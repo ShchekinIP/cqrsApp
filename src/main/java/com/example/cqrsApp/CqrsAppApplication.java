4e1354f1d9f69d13c1c7825f99ddf57a40cbbfe7
Ilya Shchekin <i.shchekin@astondevs.ru>
2024-07-03 23:56:32 +0400

    first commit

---
+++ src/main/java/com/example/cqrsApp/CqrsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsAppApplication.class, args);
+	}
+
+}
